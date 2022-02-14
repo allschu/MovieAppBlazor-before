f279398d885eabf14253cf184ea38ce0b58b8063
Allard Schuurmans <allard.schuurmans@gmail.com>
2022-02-14 15:31:47 +0100

    add service

---
+++ MovieAppBlazor/Data/CastService.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MovieAppBlazor.Data
+{
+    public class CastService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        public CastService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri(_configuration["CastFunctionApiUrl"]);
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<ICollection<Cast>> GetCastAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var response = await _httpClient.GetAsync($"GetMovieCastFunction?id={id}", cancellationToken).ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // _logger.LogError($"HTTP ERROR: {response.ReasonPhrase}");
+            }
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var resultSelection = JsonConvert.DeserializeObject<CastResultSelection>(content);
+
+            return resultSelection.cast;
+        }
+    }
+}
